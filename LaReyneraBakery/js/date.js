7d9bb800365114b0e1019f913c109e924ff5e3a3
OmarGarza01 <omarrlg@gmail.com>
2022-02-18 09:07:54 -0600

    comit

---
+++ LaReyneraBakery/js/date.js
@@ -0,0 +1,10 @@
+// get current date
+const event = new Date();
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+};
+const date = event.toLocaleDateString(undefined, options);
+document.getElementById("currentDate").innerHTML = date;
